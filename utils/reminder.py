e375f1c6537e52acd1fa0f802c82d9b23c9ce8dd
Fairoz Faisal <104089257+Fairoz007@users.noreply.github.com>
2024-10-15 17:43:27 +0530

    Delete utils directory

--- utils/reminder.py
+++
@@ -1,7 +0,0 @@
-
-from twilio.rest import Client
-
-def send_payment_reminder(phone_number, card_name, due_date):
-    client = Client("TWILIO_SID", "TWILIO_AUTH_TOKEN")
-    message = f"Reminder: Your credit card {card_name} bill is due on {due_date}."
-    client.messages.create(to=phone_number, from_="TWILIO_PHONE_NUMBER", body=message)
